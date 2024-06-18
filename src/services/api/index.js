b9e52f59ff14e943fdcbbbbf894a12c7dfc7f8c9
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-06-18 15:26:25 +0800

    feat (services/api): add interceptprs to set Bearer token in headers.

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -4,4 +4,13 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+export const setupTokenInterceptors = (token) => {
+  if (token) {
+    api.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    });
+  }
+};
+
 export default api;
