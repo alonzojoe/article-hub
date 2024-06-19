0168d7d8cc0fdc801e120768f8db2c8d2383eb85
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-06-19 23:29:52 +0800

    feat: create new slice for authenticated user in redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./slices/uiSlice";
-
+import authSlice from "./slices/authSlice";
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
+    auth: authSlice.reducer,
   },
 });
 
---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload.user;
+    },
+  },
+});
+
+export const { setUser } = authSlice.actions;
+
+export default authSlice;
