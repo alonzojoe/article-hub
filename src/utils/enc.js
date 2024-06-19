9e35d6cedc126408ed26ea15ab0ffa8447d93d36
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-06-19 23:47:09 +0800

    feat: package installed cryotojs and create service for encryption/decryption of data

---
+++ src/utils/enc.js
@@ -0,0 +1,23 @@
+import CryptoJS from "crypto-js";
+
+const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
+
+export const encryptData = (data) => {
+  try {
+    return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
+  } catch (error) {
+    console.error("Encryption error:", error);
+    return null;
+  }
+};
+
+export const decryptData = (encrypted) => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(encrypted, SECRET_KEY);
+    const decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+    return decryptedText;
+  } catch (error) {
+    console.error("Decryption error:", error);
+    return null;
+  }
+};
