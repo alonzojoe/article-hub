a8dc98707266d36706d2462b4bcc14dd5d88c447
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-06-22 10:22:30 +0800

    feat(security): Manage ProtectedRoutes logic,  by calling /auth/session endpoint and handles the showing of Outlet / redirect to login if token is not valid anymore

--- src/middleware/ProtectedRoutes.jsx
+++ src/middleware/ProtectedRoutes.jsx
@@ -8,26 +8,38 @@ import { decryptData } from "../utils/enc";
 import { getLocalStorage } from "../utils/storageActions";
 import { useDispatch } from "react-redux";
 import { setUser } from "../store/slices/authSlice";
+import useApi from "../hooks/useApi";
 const ProtectedRoutes = () => {
   const dispatch = useDispatch();
+
+  const { isLoading, error, callApi } = useApi({
+    url: "/auth/session",
+    method: "GET",
+    onSuccess: (data) => {},
+    onFailure: (error) => {},
+  });
+
   const auth = Cookie.get(import.meta.env.VITE_AUTH_KEY);
   useEffect(() => {
-    if (auth) {
-      setupTokenInterceptors(auth);
-    }
-    console.log("auth", auth);
-  });
+    if (auth) setupTokenInterceptors(auth);
+    callApi();
+  }, [auth]);
 
   const [setAppTheme] = useTheme();
   const authUser = getLocalStorage(import.meta.env.VITE_AUTH_USER);
+
   useEffect(() => {
     setAppTheme();
-    const userData = JSON.parse(decryptData(authUser));
-
-    dispatch(setUser({ user: userData }));
+    if (authUser) {
+      const userData = JSON.parse(decryptData(authUser));
+      dispatch(setUser({ user: userData }));
+    }
   }, [setAppTheme, authUser, dispatch]);
 
-  return auth ? <Outlet /> : <Navigate to="/login" />;
+  if (isLoading) return <Loader />;
+  if (error || !auth) return <Navigate to="/login" />;
+
+  return <Outlet />;
 };
 
 export default ProtectedRoutes;
