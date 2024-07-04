3a16ddf0f830fa1761541b69c2a2e251ad2cf49f
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-07-04 16:37:39 +0800

    feat: UI UX for Update Profile

--- src/layouts/components/NavbarItems/AccountDetails.jsx
+++ src/layouts/components/NavbarItems/AccountDetails.jsx
@@ -5,6 +5,11 @@ import Avatar from "../../../components/Avatar";
 import { useNavigate } from "react-router-dom";
 import api from "../../../services/api";
 import { encryptData } from "../../../utils/enc";
+import Modal from "../../../components/Modal";
+import Card from "../../../components/Card";
+import defaultProfile from "../../../assets/images/avatars/user-default.jpg";
+import Input from "../../../components/Input";
+import Button from "../../../components/Button";
 const AccountDetails = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -38,99 +43,193 @@ const AccountDetails = () => {
   };
 
   return (
-    <div
-      className="collapse navbar-collapse justify-content-end"
-      id="navbarNav"
-    >
-      <div className="d-flex align-items-center justify-content-between">
-        <a
-          href="#"
-          className="nav-link d-flex d-lg-none align-items-center justify-content-center"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#mobilenavbar"
-          aria-controls="offcanvasWithBothOptions"
-        ></a>
-        <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-center">
-          <li className="nav-item">
-            <a
-              className="nav-link nav-icon-hover cursor-pointer"
-              onClick={toggleTheme}
-            >
-              <i className={themeIcon}></i>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => viewProfile(user?.id)}>
-              <h5 className="mt-2 fw-semibold">{user?.name}</h5>
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link pe-0"
-              href="#"
-              id="drop1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <div className="d-flex align-items-center">
-                <div className="user-profile-img">
-                  <Avatar
-                    className="rounded-circle"
-                    width="35"
-                    height="35"
-                    alt="user"
-                  />
-                </div>
-              </div>
-            </a>
-            <div
-              className="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up"
-              aria-labelledby="drop1"
-            >
-              <div
-                className="profile-dropdown position-relative"
-                data-simplebar
+    <>
+      <Modal>
+        <Card>
+          <ul
+            className="nav nav-pills user-profile-tab"
+            id="pills-tab"
+            role="tablist"
+          >
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link position-relative rounded-0 d-flex align-items-center justify-content-center bg-transparent fs-3 py-3"
+                id="pills-account-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-account"
+                type="button"
+                role="tab"
+                aria-controls="pills-account"
+                aria-selected="false"
+                tabindex="-1"
               >
-                <div className="d-flex justify-content-between align-items-center py-3 px-7 pb-0">
-                  <h5 className="mb-0 fs-5 fw-semibold">User Profile</h5>
-                  <button
-                    type="button"
-                    className="btn mb-1 waves-effect waves-light btn-outline-dark btn-sm"
-                  >
-                    <i className="ti ti-settings"></i>
-                    Settings
-                  </button>
-                </div>
+                <i className="ti ti-user-circle me-2 fs-6"></i>
+                <span className="d-none d-md-block">Account</span>
+              </button>
+            </li>
 
-                <div className="d-flex align-items-center py-9 mx-7 border-bottom">
-                  <Avatar
-                    className="rounded-circle"
-                    width="70"
-                    height="70"
-                    alt="user"
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link position-relative rounded-0 d-flex align-items-center justify-content-center bg-transparent fs-3 py-3"
+                id="pills-security-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-security"
+                type="button"
+                role="tab"
+                aria-controls="pills-security"
+                aria-selected="false"
+                tabindex="-1"
+              >
+                <i className="ti ti-lock me-2 fs-6"></i>
+                <span className="d-none d-md-block">Security</span>
+              </button>
+            </li>
+          </ul>
+          <div className="profile">
+            <div className=" w-100 border position-relative overflow-hidden">
+              <div className="p-4">
+                <h4>Update Profile</h4>
+                <div className="text-center">
+                  <img
+                    src={defaultProfile}
+                    alt="modernize-img"
+                    className="img-fluid rounded-circle"
+                    width="120"
+                    height="120"
                   />
-                  <div className="ms-3">
-                    <h5 className="mb-1 fs-3">{user?.name}</h5>
-                    <p className="mb-0 d-flex text-dark align-items-center fs-2 gap-1">
-                      <i className="ti ti-mail fs-3"></i> {user?.email}
-                    </p>
+                  <div className="d-flex align-items-center justify-content-center my-4 gap-6">
+                    <button className="btn btn-primary">Upload</button>
+                    <button className="btn bg-danger-subtle text-danger">
+                      Reset
+                    </button>
                   </div>
+                  <p className="mb-3">
+                    Allowed JPG, GIF or PNG. Max size of 800K
+                  </p>
                 </div>
-                <div className="d-grid py-4 px-7 pt-8">
-                  <button
-                    className="btn btn-sm btn-outline-danger"
-                    onClick={logoutHandler}
-                  >
-                    Log Out
-                  </button>
+                <div className="row">
+                  <div className="col-12">
+                    <div className="mb-4">
+                      <label for="exampleInputtext" className="form-label">
+                        Name
+                      </label>
+                      <Input
+                        type="text"
+                        className="form-control"
+                        id="exampleInputtext"
+                        placeholder="Mathew Anderson"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <Button className="btn-primary w-100">Update</Button>
+                  </div>
                 </div>
               </div>
             </div>
-          </li>
-        </ul>
+          </div>
+        </Card>
+      </Modal>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
+        <div className="d-flex align-items-center justify-content-between">
+          <a
+            href="#"
+            className="nav-link d-flex d-lg-none align-items-center justify-content-center"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#mobilenavbar"
+            aria-controls="offcanvasWithBothOptions"
+          ></a>
+          <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-center">
+            <li className="nav-item">
+              <a
+                className="nav-link nav-icon-hover cursor-pointer"
+                onClick={toggleTheme}
+              >
+                <i className={themeIcon}></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => viewProfile(user?.id)}>
+                <h5 className="mt-2 fw-semibold">{user?.name}</h5>
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link pe-0"
+                href="#"
+                id="drop1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <div className="d-flex align-items-center">
+                  <div className="user-profile-img">
+                    <Avatar
+                      className="rounded-circle"
+                      width="35"
+                      height="35"
+                      alt="user"
+                    />
+                  </div>
+                </div>
+              </a>
+              <div
+                className="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up"
+                aria-labelledby="drop1"
+              >
+                <div
+                  className="profile-dropdown position-relative"
+                  data-simplebar
+                >
+                  <div
+                    data-v-c4be3a62=""
+                    className="d-flex justify-content-between align-items-center py-3 px-7 pb-0"
+                  >
+                    <h5 data-v-c4be3a62="" className="mb-0 fs-5 fw-semibold">
+                      User Profile
+                    </h5>
+                    <button
+                      data-v-c4be3a62=""
+                      type="button"
+                      className="btn btn-rounded btn-dark btn-sm mb-1"
+                    >
+                      <i data-v-c4be3a62="" className="ti ti-settings"></i>{" "}
+                      Settings{" "}
+                    </button>
+                  </div>
+
+                  <div className="d-flex align-items-center py-9 mx-7 border-bottom">
+                    <Avatar
+                      className="rounded-circle"
+                      width="70"
+                      height="70"
+                      alt="user"
+                    />
+                    <div className="ms-3">
+                      <h5 className="mb-1 fs-3">{user?.name}</h5>
+                      <p className="mb-0 d-flex text-dark align-items-center fs-2 gap-1">
+                        <i className="ti ti-mail fs-3"></i> {user?.email}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="d-grid py-4 px-7 pt-8">
+                    <button
+                      className="btn btn-sm btn-outline-danger"
+                      onClick={logoutHandler}
+                    >
+                      Log Out
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
