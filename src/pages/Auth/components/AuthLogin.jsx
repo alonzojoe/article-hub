23657853b0e121fc2e5983bb00e9a65a79341d46
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-07-09 10:58:29 +0800

    refactor: Login form use react-form with zod and remove refs username and password

--- src/pages/Auth/components/AuthLogin.jsx
+++ src/pages/Auth/components/AuthLogin.jsx
@@ -8,8 +8,27 @@ import Cookie from "cookiejs";
 import { useNavigate } from "react-router-dom";
 import { setLocalStorage } from "../../../utils/storageActions";
 import { encryptData } from "../../../utils/enc";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const authSchema = z.object({
+  email: z
+    .string()
+    .email({ message: "Invalid email" })
+    .trim()
+    .min(1, "Email is required"),
+  password: z.string().min(1, "Password is required"),
+});
 
 const AuthLogin = ({ changeSection }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({ resolver: zodResolver(authSchema) });
+
   const email = useRef(null);
   const password = useRef(null);
   const navigate = useNavigate();
@@ -20,8 +39,7 @@ const AuthLogin = ({ changeSection }) => {
     Cookie.set(`${import.meta.env.VITE_AUTH_KEY}`, token, { expires: 365 });
     const encryptUser = encryptData(JSON.stringify(data.user));
     setLocalStorage(import.meta.env.VITE_AUTH_USER, encryptUser);
-    email.current.value = "";
-    password.current.value = "";
+    reset();
     navigate("/");
   };
 
@@ -36,35 +54,46 @@ const AuthLogin = ({ changeSection }) => {
     onFailure,
   });
 
-  const signInHandler = async (e) => {
-    e.preventDefault();
+  const signInHandler = async (formData) => {
     await callApi({
-      email: email.current.value,
-      password: password.current.value,
+      email: formData.email,
+      password: formData.password,
     });
   };
 
   return (
-    <form onSubmit={signInHandler}>
+    <form onSubmit={handleSubmit((data) => signInHandler(data))}>
       <div className="mb-3">
         <Label htmlFor="email" className="form-label">
           Email
         </Label>
-        <Input type="email" ref={email} id="email" required />
+        <Input
+          {...register("email")}
+          className={`${errors.email ? "is-invalid" : ""}`}
+          type="email"
+          id="email"
+        />
+        <div className="invalid-feedback">{errors.email?.message}</div>
       </div>
       <div className="mb-4">
         <Label htmlFor="password" className="form-label">
           Password
         </Label>
-        <Input type="password" ref={password} id="password" required />
+        <Input
+          {...register("password")}
+          className={`${errors.email ? "is-invalid" : ""}`}
+          type="password"
+          id="password"
+        />
+        <div className="invalid-feedback">{errors.password?.message}</div>
       </div>
       <div className="d-flex align-items-center justify-content-end mb-4">
         <a className="text-primary fw-medium" href="#">
           Forgot Password ?
         </a>
       </div>
-      <Button className="btn-primary w-100 mb-4" disabled={isLoading}>
-        {isLoading ? "Signing In..." : "Sign In"}
+      <Button className="btn-primary w-100 mb-4" disabled={isSubmitting}>
+        {isSubmitting ? "Signing In..." : "Sign In"}
       </Button>
       <div className="d-flex align-items-center justify-content-center">
         <p className="fs-4 mb-0 fw-medium">New to ArticleHub?</p>
