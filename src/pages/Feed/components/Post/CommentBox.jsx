b4560be3d6cb0826315650cf6755db1839c161f4
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-07-13 11:21:52 +0800

    feat: Add data validation in CommentBox

--- src/pages/Feed/components/Post/CommentBox.jsx
+++ src/pages/Feed/components/Post/CommentBox.jsx
@@ -1,8 +1,23 @@
 import Avatar from "../../../../components/Avatar";
 import Input from "../../../../components/Input";
+import { useForm } from "react-hook-form";
 const CommentBox = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm();
+
+  const submitComment = (data) => {
+    console.log("comment", data.comment);
+  };
+
   return (
-    <div className="d-flex align-items-center gap-3 p-3">
+    <form
+      className="d-flex align-items-center gap-3 p-3"
+      onSubmit={handleSubmit((data) => submitComment(data))}
+    >
       <Avatar
         alt="user-logo"
         className="rounded-circle"
@@ -10,13 +25,20 @@ const CommentBox = () => {
         height="35"
       />
       <Input
+        {...register("comment", {
+          required: true,
+          minLength: {
+            value: 1,
+            message: "Comment is required",
+          },
+        })}
         type="text"
-        className="form-control-sm py-8"
+        className={`form-control-sm py-8 ${errors.comment ? "is-invalid" : ""}`}
         id="comment-box"
         placeholder="Comment"
       />
       <button className="btn btn-primary btn">Comment</button>
-    </div>
+    </form>
   );
 };
 
