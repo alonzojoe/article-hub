4cfe68cec19299fb5f9dc5749bae5fc56212e953
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-07-27 13:33:23 +0800

    refactor: Upvoting and Downvoting logic, apply the redures for both method with api call each actions

--- src/pages/Feed/components/Post/PostEngagements.jsx
+++ src/pages/Feed/components/Post/PostEngagements.jsx
@@ -1,58 +1,80 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import { getPost } from "../../../../store/thunks/postsThunks";
+import moment from "moment";
+import { postActions } from "../../../../store/slices/postSlice";
 import api from "../../../../services/api";
 import toast from "react-hot-toast";
 
 const PostEngagements = ({ post, toggle: toggleTrue, onSelect, selected }) => {
+  const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const [vote, setVote] = useState(false);
-  const [currentCount, setCurrentCount] = useState(post.votes.length);
+  const voteChecker = post.votes.some((vote) => vote.user_id === user.id);
+
   const viewPost = async () => {
     onSelect();
     toggleTrue(true);
     dispatch(getPost(post.id));
   };
 
-  const { user } = useSelector((state) => state.auth);
+  const castVote = async () => {
+    const dateNow = moment().toISOString();
+    const voteDetails = {
+      article_id: post.id,
+      created_at: dateNow,
+      id: Date.now(),
+      udated_at: dateNow,
+      user_id: user.id,
+    };
 
-  const upVote = async () => {
-    try {
-      await api.post("/cast/vote", {
-        article_id: post.id,
-        user_id: user.id,
-      });
-      setVote(true);
-      setCurrentCount((prev) => prev + 1);
-    } catch (error) {
-      toast.error("An error occured");
+    console.log(voteDetails);
+
+    if (!voteChecker) {
+      dispatch(postActions.upVote({ voteDetails }));
+      try {
+        await api.post("/cast/vote", {
+          article_id: post.id,
+          user_id: user.id,
+        });
+      } catch (error) {
+        toast.error("An error occured");
+        dispatch(postActions.upVote({ voteDetails }));
+      }
+    } else {
+      dispatch(postActions.downVote({ voteDetails }));
+      try {
+        await api.delete(`/cast/down-vote/${voteDetails.article_id}`, {
+          params: {
+            user_id: user.id,
+          },
+        });
+      } catch (error) {
+        toast.error("An error occured");
+        dispatch(postActions.upVote({ voteDetails }));
+      }
     }
   };
 
-  const conditioned =
-    vote || post.votes.some((vote) => vote.user_id === user.id);
-
-  const voteIcon = conditioned ? "bxs-upvote" : "bx-upvote";
+  const voteIcon = voteChecker ? "bxs-upvote" : "bx-upvote";
 
   return (
     <div className="d-flex align-items-center my-3">
       {!selected && (
         <div className="d-flex align-items-center gap-2">
           <a
-            className={`d-flex align-items-center justify-content-center text-bg-primary p-2 fs-4 rounded-circle ${
-              conditioned ? "pe-none" : ""
-            }`}
+            className={`d-flex align-items-center justify-content-center text-bg-primary p-2 fs-4 rounded-circle`}
             data-bs-toggle="tooltip"
             data-bs-placement="top"
             data-bs-title="Like"
-            onClick={upVote}
+            onClick={castVote}
           >
             <i className={`bx ${voteIcon}`}></i>
           </a>
-          <span className="text-dark fw-semibold">{currentCount}</span>
+          <span className="text-dark fw-semibold">{post.votes.length}</span>
         </div>
       )}
-
+      {/* {`Vote Check : ${voteChecker}`}
+      {`Vote : ${JSON.stringify(post.votes)}`} */}
       {!selected && (
         <div className="d-flex align-items-center gap-2 ms-4">
           <a
