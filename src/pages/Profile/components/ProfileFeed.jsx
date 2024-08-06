339b542cb31d63bc036eb7750969a6e1d57f8f51
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-08-06 16:55:15 +0800

    feat: Implement viewComments on User Profile News Feed

--- src/pages/Profile/components/ProfileFeed.jsx
+++ src/pages/Profile/components/ProfileFeed.jsx
@@ -4,6 +4,9 @@ import Post from "../../Feed/components/Post/Post";
 import SkeletonPosts from "../../Feed/Skeletons/SkeletonPosts";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserPosts } from "../../../store/thunks/postsThunks";
+import Modal from "../../../components/Modal";
+import useToggle from "../../../hooks/useToggle";
+import SelectedPost from "../../Feed/components/Post/SelectedPost";
 const ProfileFeed = ({ userId }) => {
   const dispatch = useDispatch();
   const { items, isLoading, error } = useSelector((state) => state.posts);
@@ -11,6 +14,14 @@ const ProfileFeed = ({ userId }) => {
   useEffect(() => {
     dispatch(fetchUserPosts(userId));
   }, [dispatch]);
+
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [value, toggle] = useToggle(false);
+  const viewPostTitle = `${selectedPost?.user?.name}'s Post`;
+
+  const viewComments = (id) => {
+    toggle(true);
+  };
 
   let errorMessage;
   if (error) {
@@ -23,6 +34,15 @@ const ProfileFeed = ({ userId }) => {
   }
   return (
     <>
+      {value && (
+        <Modal
+          title={viewPostTitle}
+          footer={<CommentBox postId={selectedPost.id} />}
+          onClose={() => toggle(false)}
+        >
+          <SelectedPost post={selectedPost} />
+        </Modal>
+      )}
       <FeedContainer>
         {errorMessage}
         {!isLoading ? (
